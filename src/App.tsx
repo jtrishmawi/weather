ee9e5d158894ba3744da7a60e2c27252e2c8f0d8
Tarek <jtrishmawi@gmail.com>
2024-03-24 22:33:37 +0100

    add 1st graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
 import { useGeolocation } from "@/hooks/useGeolocation";
 import { useWeather } from "./hooks/useWeather";
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export const App = () => {
   const state = useGeolocation();
   const { data, isLoading, isError } = useWeather(state);
-console.log(data)
-  if (isLoading) {
+
+  if (state.loading || isLoading) {
     return <p>loading... (you may need to enable permissions)</p>;
   }
 
-  if (isError) {
+  if (state.error || isError) {
     return <p>Enable permissions to access your location data</p>;
   }
 
-  return <div>App</div>;
+  const charts = data?.hourly.time.map((time, index) => {
+    return {
+      rain: data?.hourly.rain[index] === 0 ? null : data?.hourly.rain[index],
+      temperature: data?.hourly.temperature2m[index],
+      time: time,
+    };
+  });
+
+  return (
+    <div className="w-full h-screen">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={500} height={300} data={charts}>
+          <XAxis dataKey={"time"} />
+          <YAxis yAxisId={"rain"} orientation="right" />
+          <YAxis yAxisId={"temperature"} />
+          <Tooltip />
+          <Legend />
+          <Line dataKey={"rain"} yAxisId={"rain"} stroke="#8884d8" />
+          <Line
+            dataKey={"temperature"}
+            yAxisId={"temperature"}
+            stroke="#82ca9d"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
