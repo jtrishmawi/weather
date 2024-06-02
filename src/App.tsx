48defd57f08876fd36110afa1bb003692d22f085
Tarek <jtrishmawi@gmail.com>
2024-06-02 23:15:21 +0200

    current card and compass

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import { Forecast } from "@/components/forecast";
 import { Map } from "@/components/map";
 import { ModeToggle } from "@/components/mode-toggle";
 import { OverviewChart } from "@/components/overview-chart";
-import { useGeolocation } from "@/hooks/useGeolocation";
-import { useReverseGeocoding } from "@/hooks/useReverseGeocoding";
-import { useWeatherApi } from "@/hooks/useWeatherApi";
-import { Forecast } from "@/components/forecast";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import { getWMOCode, getWMOImageUrl } from "./lib/wmo-codes";
+import { useGeolocation } from "@/hooks/useGeolocation";
+import { useReverseGeocoding } from "@/hooks/useReverseGeocoding";
+import { useWeatherApi } from "@/hooks/useWeatherApi";
+import { CurrentCard } from "./components/current-card";
 
 export const App = () => {
   const state = useGeolocation();
@@ -41,6 +41,10 @@ export const App = () => {
   if (geocoding.isError) {
     return <p>Geocoding error</p>;
   }
+
+  if (!weather.data || !geocoding.data) {
+    return <p>No data</p>;
+  }
   console.log(weather.data?.current);
   return (
     <div className="w-full h-screen flex flex-col px-8 pb-4">
@@ -53,50 +57,10 @@ export const App = () => {
         <ResizablePanel>
           <ResizablePanelGroup direction="horizontal">
             <ResizablePanel className="rounded-xl border-2 dark:bg-neutral-800 bg-neutral-100">
-              <h2 className="scroll-m-20 m-2 text-3xl font-semibold tracking-tight first:mt-0">
-                {geocoding.data?.city}, {geocoding.data?.countryName}
-              </h2>
-              {weather.data?.current && (
-                <div>
-                  <div>
-                    <img
-                      src={getWMOImageUrl(
-                        weather.data?.current.weatherCode.toString(),
-                        weather.data?.current.isDay ? "day" : "night"
-                      )}
-                      alt={getWMOCode(
-                        weather.data?.current.weatherCode.toString()
-                      )}
-                    />
-                    {weather.data?.current.temperature2m.toFixed(1)}&deg;C /
-                    feels like:&nbsp;
-                    {weather.data?.current.apparentTemperature.toFixed(1)}&deg;C
-                  </div>
-                  <div>
-                    <div>
-                      Humidity: {weather.data?.current.relativeHumidity2m}%
-                    </div>
-                    <div>Rain: {weather.data?.current.rain}mm</div>
-                    <div>
-                      Precipitation: {weather.data?.current.precipitation} mm
-                    </div>
-                    <div>Showers: {weather.data?.current.showers}mm</div>
-                    <div>Snowfall: {weather.data?.current.snowfall}cm</div>
-                    <div>Cloud cover: {weather.data?.current.cloudCover}%</div>
-                    <div>
-                      Wind: {weather.data?.current.windSpeed10m.toFixed(1)} km/h
-                    </div>
-                    <div>
-                      Wind direction:{" "}
-                      {weather.data?.current.windDirection10m.toFixed(1)}&deg;
-                    </div>
-                    <div>
-                      Wind gusts:{" "}
-                      {weather.data?.current.windGusts10m.toFixed(1)} km/h
-                    </div>
-                  </div>
-                </div>
-              )}
+              <CurrentCard
+                geocoding={geocoding.data!}
+                weather={weather.data!}
+              />
             </ResizablePanel>
             <ResizableHandle withHandle />
             <ResizablePanel className="rounded-xl border-2 overflow-hidden dark:bg-neutral-800 bg-neutral-100">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 // Helper function to form time ranges
 export const range = (start: number, stop: number, step: number) =>
   Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+export const getCardinalDirection = (deg: number) => {
+  const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+  return directions[Math.round(deg / 45) % 8];
+};
