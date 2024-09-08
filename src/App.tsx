1089174408c3beab166428fd9394f59a72486977
Tarek <jtrishmawi@gmail.com>
2024-09-08 14:47:09 +0200

    better geolocation handling

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,68 @@ import { useGeolocation } from "@/hooks/useGeolocation";
 import { useReverseGeocoding } from "@/hooks/useReverseGeocoding";
 import { useWeatherApi } from "@/hooks/useWeatherApi";
 import { CurrentCard } from "./components/current-card";
+import { Button } from "./components/ui/button";
 
 export const App = () => {
-  const state = useGeolocation();
-  const weather = useWeatherApi(state);
-  const geocoding = useReverseGeocoding(state);
+  const geolocation = useGeolocation();
+  const weather = useWeatherApi(geolocation.data!);
+  const geocoding = useReverseGeocoding(geolocation.data!);
 
-  if (state.loading) {
+  if (geolocation.isLoading) {
     return (
-      <p>Geolocation is loading... (you may need to enable permissions)</p>
+      <p className="w-full h-screen flex items-center justify-center gap-4">
+        Geolocation is loading...
+        <br />
+        (you may need to enable permissions)
+      </p>
     );
   }
   if (weather.isLoading) {
-    return <p>Weather is loading...</p>;
+    return (
+      <p className="w-full h-screen flex items-center justify-center gap-4">
+        Weather is loading...
+      </p>
+    );
   }
   if (geocoding.isLoading) {
-    return <p>Geocoding is loading...</p>;
+    return (
+      <p className="w-full h-screen flex items-center justify-center gap-4">
+        Geocoding is loading...
+      </p>
+    );
   }
 
-  if (state.error && state.latitude === null && state.longitude === null) {
-    console.error(state.error);
-    return <p>Enable permissions to access your location data</p>;
+  if (geolocation.isError) {
+    return (
+      <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        <p>Enable permissions to access your location data</p>
+        <Button onClick={() => window.location.reload()}>Retry</Button>
+      </div>
+    );
   }
 
-  if (weather.isError || geocoding.isError) {
-    return <p>Weather error</p>;
+  if (weather.isError) {
+    return (
+      <p className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        Weather error...
+      </p>
+    );
   }
 
   if (geocoding.isError) {
-    return <p>Geocoding error</p>;
+    return (
+      <p className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        Geocoding error...
+      </p>
+    );
   }
 
-  if (!weather.data || !geocoding.data) {
-    return <p>No data</p>;
+  if (geolocation.isIdle || weather.isIdle || geocoding.isIdle) {
+    return (
+      <p className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        Loading data...
+      </p>
+    );
   }
 
   return (
--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -1,16 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useQuery } from "react-query";
 
-type GeolocationState = {
-  loading: boolean;
-  accuracy: number | null;
-  altitude: number | null;
-  altitudeAccuracy: number | null;
-  heading: number | null;
+type GeolocationParams = {
   latitude: number | null;
   longitude: number | null;
-  speed: number | null;
-  timestamp: number | null;
-  error?: GeolocationPositionError | null;
 };
 
 export const useGeolocation = (
@@ -20,60 +12,29 @@ export const useGeolocation = (
     maximumAge: 3600000,
   }
 ) => {
-  const [state, setState] = useState<GeolocationState>({
-    loading: true,
-    accuracy: null,
-    altitude: null,
-    altitudeAccuracy: null,
-    heading: null,
-    latitude: null,
-    longitude: null,
-    speed: null,
-    timestamp: null,
-    error: null,
-  });
-
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    const onEvent = ({ coords, timestamp }: GeolocationPosition) => {
-      setState({
-        loading: false,
-        timestamp,
-        latitude: coords.latitude,
-        longitude: coords.longitude,
-        altitude: coords.altitude,
-        accuracy: coords.accuracy,
-        altitudeAccuracy: coords.altitudeAccuracy,
-        heading: coords.heading,
-        speed: coords.speed,
+  return useQuery(
+    "geolocation",
+    () => {
+      return new Promise<GeolocationParams>((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            resolve({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          (error) => {
+            reject(error);
+          },
+          options
+        );
       });
-    };
-
-    const onEventError = (error: GeolocationPositionError) => {
-      setState((s) => ({
-        ...s,
-        loading: false,
-        error,
-      }));
-    };
-
-    navigator.geolocation.getCurrentPosition(
-      onEvent,
-      onEventError,
-      optionsRef.current
-    );
-
-    const watchId = navigator.geolocation.watchPosition(
-      onEvent,
-      onEventError,
-      optionsRef.current
-    );
-
-    return () => {
-      navigator.geolocation.clearWatch(watchId);
-    };
-  }, []);
-
-  return state;
+    },
+    {
+      placeholderData: {
+        latitude: null,
+        longitude: null,
+      },
+    }
+  );
 };
