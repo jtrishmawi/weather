dceac9683caecbea18097aaac7940c7922ad9c1d
Tarek <jtrishmawi@gmail.com>
2025-05-24 18:08:01 +0200

    implement build visualizer and chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
+
+const isVercel = process.env.VERCEL === "1";
 
 export default defineConfig({
   plugins: [react(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: isVercel ? [] : [visualizer({ open: true })],
+      output: {
+        manualChunks: {
+          leaflet: ["leaflet", "react-leaflet"],
+          recharts: ["recharts"],
+        },
+      },
     },
   },
 });
